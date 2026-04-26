2bb80431858734d77e3b428291fb2efc9ceaec84
Andrez18 <ieca.andresgomez@gmail.com>
2026-04-26 18:38:53 -0500

    corregir la URL del icono de 192x192 a formato PNG en el metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   authors: [{ name: 'Jorge Vallejo' }],
   icons: {
     icon: [
-      { url: '/icon-192x192.jpg', sizes: '192x192', type: 'image/jpeg' },
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/jpeg' },
       { url: '/icon-512x512.jpg', sizes: '512x512', type: 'image/jpeg' },
     ],
     apple: '/apple-icon.jpg',
